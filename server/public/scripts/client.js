a692841d0e1fd801c1edbd2dcf37e7ebde9e68ea
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-19 17:51:48 -0600

    moved to home

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,43 +12,40 @@ $(document).ready(function(){
       phirephitersArray = data.phirephiters; // NOTE: no var=global
       // displayAllData(); // NOTE: Calls function displayData
       displayDataAtIndex();
-    } // NOTE: FOR: success: function(data){
+  } // NOTE: FOR: success: function(data){
   }); // NOTE: FOR: $.ajax({
 
 }); // NOTE: FOR: $(document).ready(function(){
 
 // NOTE: Option 01: Complete array remains in RAM & is called to DOM one object at a time.
-function displayDataAtIndex (){
+  function displayDataAtIndex (){
+    appendDOM(phirephitersArray[peopleIndex]);
     // console.log(phirephitersArray); // NOTE: all objects
     // console.log(phirephitersArray[13]); // NOTE: one object
-  appendDOM(phirephitersArray[peopleIndex]);
-}  // NOTE: FOR: function displayData (){
+  }  // NOTE: FOR: function displayData (){
 
-//   $('#fwdButton').on('click', function() {
-// }) // NOTE: FOR: $('#fwdButton').on('click', function() {
+  // NOTE: Option 02:  Complete array kept on DOM & cycled through
+  // function displayAllData (){
+  //     // console.log(phirephitersArray); // NOTE: all objects
+  //     // console.log(phirephitersArray[13]); // NOTE: one object
+  //     // $('#divBottom').children().last().append('<li>' + phirephitersArray[10] + '</li>');
+  //     for (var i = 0; i < phirephitersArray.length; i++) {
+  //       appendDOM(phirephitersArray[i]);
+  //     }  // NOTE: FOR: for (var i = 0; i < phirephitersArray.length; i++) {
+  // }  // NOTE: FOR: function displayData (){
 
-// click the next button
-// move to the next array index
-// call the function
-
-// NOTE: Option 02:  Complete array kept on DOM & cycled through
-// function displayAllData (){
-//     // console.log(phirephitersArray); // NOTE: all objects
-//     // console.log(phirephitersArray[13]); // NOTE: one object
-//     // $('#divBottom').children().last().append('<li>' + phirephitersArray[10] + '</li>');
-//     for (var i = 0; i < phirephitersArray.length; i++) {
-//       appendDOM(phirephitersArray[i]);
-//     }  // NOTE: FOR: for (var i = 0; i < phirephitersArray.length; i++) {
-// }  // NOTE: FOR: function displayData (){
-
-
-function appendDOM(ulPeople) { // anyFunctionName
+  function appendDOM(ulPeople) {  // NOTE: Function appending to DOM from AJAX
     $('#divMain').append('<ul class="ulPeople"></ul>');
     var $addDivMain = $('#divMain').children().last();
-    var $addDivBottom = $('#divBottom').children().last();
+    // var $addDivBottom = $('#divBottom').children().last();
     $addDivMain.append('<li>' + ulPeople.name + '</li>');
     $addDivMain.append('<li>' + ulPeople.git_username + '</li>');
     $addDivMain.append('<li>' + ulPeople.shoutout + '</li>');
+    //   $('#fwdButton').on('click', function() {
+    // }) // NOTE: FOR: $('#fwdButton').on('click', function() {
+    for (var i = 0; i < phirephitersArray.length; i++) {
+      $('#mainImg').replaceWith().phirephitersArray[i]
+    } // NOTE: FOR for loop
     // console.log($addDivMain); // Returning successfully
     // $addDivBottom.append('<li>' + phirephitersArray[3] + '</li>'); DELETE? // NOTE: This runs through for Loop
-} // NOTE: FOR: function appendDOM(ulPeople) {
+  } // NOTE: FOR: function appendDOM(ulPeople) {
