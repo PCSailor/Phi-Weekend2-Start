90c3baccdf2cb0c76eacd7d52aea302cb443d748
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-19 15:06:12 -0600

    working on DOM-Deleted Commented-out code next

---
+++ Pseudo-Code.md
@@ -0,0 +1,13 @@
+BASE MODE
+1) AJAX method GET request from client side app.js, accessing the /data URL, & bringing data back down
+2) On the DOM
+    -One person display shows their name, their Github link, and their shoutout feedback
+    -Series index data array with all people with the highlighted person's index called out
+    -'Next' & 'Previous' buttons navigating to the respective person
+    -Highlighted index should update as you click to other people
+
+HARD MODE
+Include a fade out and fade in animation in-between transitioning people
+
+PRO MODE
+Include a timer that moves to the next person if the user is not clicking on next or prev. If the user clicks on next or prev, the timer should be reset. The timer should transition between people every 10 seconds.
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,22 +2,21 @@
 // add next button
 // add previous button
 console.log('Make sure youre on http://localhost:5000/' );
-
+var phirephitersArray;
 
 
 $(document).ready(function(){
   console.log('jQuery is ready');
   // Upon page load, get the data from the server
-  $.ajax({
+  $.ajax({ // function of jQuery
     url: '/data',
     method: 'GET',
     success: function(data){
       console.log('returned data from server: ', data);
-      // NOTE: Successful to the DOM with appendDOM(data.phirephiters[1]);
-      for (var i = 0; i < data.phirephiters.length; i++) {
-        appendDOM(data.phirephiters[i]);
-            console.log(appendDOM(data.phirephiters[i]));
-      }
+      phirephitersArray = data.phirephiters; // no var global
+
+      displayData();
+
 
 
       // QUESTION: How do you get one piece of info off the server? i.e.:  Object=phirephiters Array[0] / Object  Name ??  // NOTE: $('.test').data.phirephiters.name;   // test();
@@ -25,131 +24,164 @@ $(document).ready(function(){
     } // NOTE: FOR: success: function(data){
     }); // NOTE: FOR: $.ajax({
 
-      var test01 = function(){
-        $('.listFoot').text('list-02');} // Appends to DOM
-        test01(); // NOTE: Calls appends to DOM function
+      function displayData (){
+        // console.log(phirephitersArray); // NOTE: Working for all objects
+        // console.log(phirephitersArray[13]); // NOTE: Working for individual object
+        $('#divBottom').children().last().append('<li>' + phirephitersArray[10] + '</li>');
 
-        function appendDOM(ulPeople) { // anyFunctionName
-          $('#divMain').append('<ul class="ulPeople"></ul>');
-          var $addKeys = $('#divMain').children().last();
-          $addKeys.append('<li>' + ulPeople.name + '</li>');
-          $addKeys.append('<li>' + ulPeople.git_username + '</li>');
-          // $addKeys.append('<li>' + ulPeople.shoutout + '</li>');
-          $addKeys.append('<li>' + ulPeople.shoutout + '</li>');
-        }
-        $('#divMain:eq()').hide(); // css('color', 'red');
-        $('#divMain:eq()').show();
-        $('li').eq(5).css('background-color', 'red');
-        $('body').find('div').eq(2).addClass('blue');
-
-        // NOTE: This is working and appending list to DOM
-        // function appendDOM(divPeople) { // anyFunctionName
-        //   $('#divMain').append('<div class="divPeople"></div>');
-        //   var $addKeys = $('#divMain').children().last();
-        //   $addKeys.append('<li>' + divPeople.name + '</li>');
-        //   $addKeys.append('<li>' + divPeople.git_username + '</li>')
-        //   $addKeys.append('<li>' + divPeople.shoutout + '</li>')
-        // }
-        // NOTE: Adding a single picture
-        // $('<img src=" '+ imgPath + '">').load(function(){
-        // $('<img src="'+ $HOME/Desktop/Fox Picture 003.jpg + '">').load(function(){
-        //   $(this).width('300px').height('300px').append('#divMain');
-        // });
+        // NOTE: Successful to the DOM with appendDOM(data.phirephiters[1]);
+        for (var i = 0; i < phirephitersArray.length; i++) {
+          appendDOM(phirephitersArray[i]);
+        }  // NOTE: FOR: for (var i = 0; i < phirephitersArray.length; i++) {
+        }  // NOTE: FOR: function displayData (){
 
 
 
 
-
-
-      }); // NOTE: FOR: $(document).ready(function(){
-
-
-
-
-
-
-
-
-
-
-
+            function appendDOM(ulPeople) { // anyFunctionName
+              $('#divMain').append('<ul class="ulPeople"></ul>');
+              var $addDivMain = $('#divMain').children().last();
+              var $addDivBottom = $('#divBottom').children().last();
+              $addDivMain.append('<li>' + ulPeople.name + '</li>');
+              $addDivMain.append('<li>' + ulPeople.git_username + '</li>');
+              // $addDivMain.append('<li>' + ulPeople.shoutout + '</li>');
+              $addDivMain.append('<li>' + ulPeople.shoutout + '</li>');
+              // console.log($addDivMain); // Returning successfully
+              $addDivBottom.append('<li>' + phirephitersArray[0] + '</li>'); // NOTE: This runs through for Loop
+            } // NOTE: FOR: function appendDOM(ulPeople) {
 
 
 
+              // QUESTION: appendDOM is a function called for within the AJAX function.  function appendDOM is called for with ulPeople but within the AJAX function the appendDOM function is called for with the data.phirephiters[i] object-array.  How does the server data transfer to the function appendDOM(ulPeople)?
 
-
-
-
-
-
-
-
+              // $('#divMain:eq()').hide(); // NOTE: Hides divMain data
+              $('#divMain').hide(); // NOTE: Hides divMain data
+              $('#divMain:eq()').show(); // NOTE: Shows divMain data
+              $('li').eq(1).css('background-color', 'red');
+              $('body').find('div').eq(0).css('background-color', 'blue');
+              // $('#divBottom')  // data.phirephiters
 
 
 
 
+              //TODO: $('.button').on('click', function(){
+              // if (on click == #backButton) {
+              //   navigate to previous image
+              // } else {
+              //   navigate to next image
+              // }
+              // });
+
+              // NOTE: This is working and appending list to DOM
+              // function appendDOM(divPeople) { // anyFunctionName
+              //   $('#divMain').append('<div class="divPeople"></div>');
+              //   var $addDivMain = $('#divMain').children().last();
+              //   $addDivMain.append('<li>' + divPeople.name + '</li>');
+              //   $addDivMain.append('<li>' + divPeople.git_username + '</li>')
+              //   $addDivMain.append('<li>' + divPeople.shoutout + '</li>')
+              // }
+              // NOTE: Adding a single picture
+              // $('<img src=" '+ imgPath + '">').load(function(){
+              // $('<img src="'+ $HOME/Desktop/Fox Picture 003.jpg + '">').load(function(){
+              //   $(this).width('300px').height('300px').append('#divMain');
+              // });
 
 
 
 
 
 
+            }); // NOTE: FOR: $(document).ready(function(){
 
-        // // get one pic on dom
-        // // add next button
-        // // add previous button
-        // console.log('Make sure youre on http://localhost:5000/' );
-        //
-        // $(document).ready(function(){
-        // console.log('jQuery is ready');
-        //     // Upon page load, get the data from the server
-        //     $.ajax({
-        //       url: '/data',
-        //       method: 'GET',
-        //       success: function(data){
-        //         console.log('returned data from server: ', data);
-        // // NOTE: Successful to the DOM with appendDOM(data.phirephiters[1]);
-        // for (var i = 0; i < data.phirephiters.length; i++) {
-        //   appendDOM(data.phirephiters[i]);
-        // }
-        // console.log(appendDOM(data.phirephiters[11]));
-        //
-        // // QUESTION: How do you get one piece of info off the server? i.e.:  Object=phirephiters Array[0] / Object  Name ??  // NOTE: $('.test').data.phirephiters.name;   // test();
-        //     // } // NOTE: FOR: function test(){
-        //       } // NOTE: FOR: success: function(data){
-        //     }); // NOTE: FOR: $.ajax({
-        //
-        // // NOTE: Testing Area
-        // // NOTE: Testing Area
-        // // NOTE: Next: change text
-        // var test01 = function(){
-        //   // $('.test').first().text('testing');} // NOTE: succssfully appends to DOM
-        //   $('.test').text('list-02');}
-        // test01(); // NOTE: This line calls the append to DOM function
-        // // NOTE: Next: Italics
-        // // $('li.test').html(function() {
-        // //   return '<em>' + $(this).text() + '</em>';
-        // // }); // NOTE: succssfully appends italics to DOM
-        // // // NOTE: Next:
-        // // $('li.name').html(function() { // div#divPeople
-        // //   return '<em>' + $(this).text() + '</em>';
-        // }); // NOTE:
-        //
-        // // NOTE: Testing Area
-        // // NOTE: Testing Area
-        //
-        // function appendDOM(divPeople) { // anyFunctionName
-        //   $('#divMain').append('<div class="divPeople"></div>');
-        //   var $addKeys = $('#divMain').children().last();
-        // //   $addKeys.append('<li class="name">' + divPeople.name + '</li>');
-        // //   $addKeys.append('<li class="gitUser">' + divPeople.git_username + '</li>')
-        // // }
-        // // NOTE: Copied working 3 lines above
-        // $addKeys.append('<li>' + divPeople.name + '</li>');
-        // $addKeys.append('<li>' + divPeople.git_username + '</li>')
-        // }// NOTE: FOR: function appendDOM(divPeople) {
-        //
-        //
-        //
-        //
-        // }); // NOTE: FOR: $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              // var test01 = function(){
+              //   $('.listFoot').text('var test01 = function(){');} // Appends to DOM
+              //   test01(); // NOTE: Calls appends to DOM function
+
+
+
+              // // get one pic on dom
+              // // add next button
+              // // add previous button
+              // console.log('Make sure youre on http://localhost:5000/' );
+              //
+              // $(document).ready(function(){
+              // console.log('jQuery is ready');
+              //     // Upon page load, get the data from the server
+              //     $.ajax({
+              //       url: '/data',
+              //       method: 'GET',
+              //       success: function(data){
+              //         console.log('returned data from server: ', data);
+              // // NOTE: Successful to the DOM with appendDOM(data.phirephiters[1]);
+              // for (var i = 0; i < data.phirephiters.length; i++) {
+              //   appendDOM(data.phirephiters[i]);
+              // }
+              // console.log(appendDOM(data.phirephiters[11]));
+              //
+              // // QUESTION: How do you get one piece of info off the server? i.e.:  Object=phirephiters Array[0] / Object  Name ??  // NOTE: $('.test').data.phirephiters.name;   // test();
+              //     // } // NOTE: FOR: function test(){
+              //       } // NOTE: FOR: success: function(data){
+              //     }); // NOTE: FOR: $.ajax({
+              //
+              // // NOTE: Testing Area
+              // // NOTE: Testing Area
+              // // NOTE: Next: change text
+              // var test01 = function(){
+              //   // $('.test').first().text('testing');} // NOTE: succssfully appends to DOM
+              //   $('.test').text('list-02');}
+              // test01(); // NOTE: This line calls the append to DOM function
+              // // NOTE: Next: Italics
+              // // $('li.test').html(function() {
+              // //   return '<em>' + $(this).text() + '</em>';
+              // // }); // NOTE: succssfully appends italics to DOM
+              // // // NOTE: Next:
+              // // $('li.name').html(function() { // div#divPeople
+              // //   return '<em>' + $(this).text() + '</em>';
+              // }); // NOTE:
+              //
+              // // NOTE: Testing Area
+              // // NOTE: Testing Area
+              //
+              // function appendDOM(divPeople) { // anyFunctionName
+              //   $('#divMain').append('<div class="divPeople"></div>');
+              //   var $addDivMain = $('#divMain').children().last();
+              // //   $addDivMain.append('<li class="name">' + divPeople.name + '</li>');
+              // //   $addDivMain.append('<li class="gitUser">' + divPeople.git_username + '</li>')
+              // // }
+              // // NOTE: Copied working 3 lines above
+              // $addDivMain.append('<li>' + divPeople.name + '</li>');
+              // $addDivMain.append('<li>' + divPeople.git_username + '</li>')
+              // }// NOTE: FOR: function appendDOM(divPeople) {
+              //
+              //
+              //
+              //
+              // }); // NOTE: FOR: $(document).ready(function(){
