7250c779080d78e02caf1b87f96c3a70795d96b2
Phil <PC@Stephanies-MacBook-Pro.local>
2017-02-20 09:07:51 -0600

    working single object on DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,56 +3,75 @@ var phirephitersArray;
 var peopleIndex = 0;
 $(document).ready(function(){
   console.log('jQuery ready');
+  // NOTE: Buttons
+  $('#backButton').on('click', function() {
+    peopleIndex--;
+    console.log(peopleIndex);
+
+    // phirephitersArray[peopleIndex];
+    // for (var i = 0; i < phirephitersArray.length; i++) {
+    // }
+    }); // NOTE: FOR: $('#backButton').on('click', function() {
+  // click back = navigate back one data person
+  // $(htmlNavAaccess).main().replaceWith(phirephitersArray[3]); // .replaceWith = HTML string, DOM element, array of DOM elements, or jQuery object.
+
+  $('#fwdButton').on('click', function() {
+    peopleIndex++;
+    displayDataAtIndex();
+    console.log(peopleIndex);
+  });
 
   $.ajax({ // function of jQuery that gets data from server
     url: '/data',
     method: 'GET',
     success: function(data){
-      // console.log('returned data from server: ', data); // NOTE: Main source showing all data
+      console.log('returned data from server: ', data); // NOTE: Main source showing all data
       phirephitersArray = data.phirephiters; // NOTE: no var=global
       // displayAllData(); // NOTE: Calls function displayData
       displayDataAtIndex();
-  } // NOTE: FOR: success: function(data){
-  }); // NOTE: FOR: $.ajax({
+    } // NOTE: FOR: success: function(data){
+    }); // NOTE: FOR: $.ajax({
 
-}); // NOTE: FOR: $(document).ready(function(){
+    }); // NOTE: FOR: $(document).ready(function(){
 
-// NOTE: Option 01: Complete array remains in RAM & is called to DOM one object at a time.
-  function displayDataAtIndex (){
-    appendDOM(phirephitersArray[peopleIndex]);
-    // console.log(phirephitersArray); // NOTE: all objects
-    // console.log(phirephitersArray[13]); // NOTE: one object
-  }  // NOTE: FOR: function displayData (){
+      // NOTE: Option 01: Complete array remains in RAM & is called to DOM one object at a time.
+      function displayDataAtIndex (){
+        appendDOM(phirephitersArray[peopleIndex]);
+        // appendDOM(ulPeople);
+      console.log(phirephitersArray); // NOTE: all objects
+      console.log(phirephitersArray[13]); // NOTE: one object
+      }  // NOTE: FOR: function displayData (){
 
-  // NOTE: Option 02:  Complete array kept on DOM & cycled through
-  // function displayAllData (){
-  //     // console.log(phirephitersArray); // NOTE: all objects
-  //     // console.log(phirephitersArray[13]); // NOTE: one object
-  //     // $('#divBottom').children().last().append('<li>' + phirephitersArray[10] + '</li>');
-  //     for (var i = 0; i < phirephitersArray.length; i++) {
-  //       appendDOM(phirephitersArray[i]);
-  //     }  // NOTE: FOR: for (var i = 0; i < phirephitersArray.length; i++) {
-  // }  // NOTE: FOR: function displayData (){
+      // NOTE: Option 02:  Complete array kept on DOM & cycled through
+      // function displayAllData (){
+      //     // console.log(phirephitersArray); // NOTE: all objects
+      //     // console.log(phirephitersArray[13]); // NOTE: one object
+      //     // $('#divBottom').children().last().append('<li>' + phirephitersArray[10] + '</li>');
+      //     for (var i = 0; i < phirephitersArray.length; i++) {
+      //       appendDOM(phirephitersArray[i]);
+      //     }  // NOTE: FOR: for (var i = 0; i < phirephitersArray.length; i++) {
+      // }  // NOTE: FOR: function displayData (){
 
-  function appendDOM(ulPeople) {  // NOTE: Function appending to DOM from AJAX
-    $('#divMain').append('<ul class="ulPeople"></ul>');
-    var $addDivMain = $('#divMain').children().last();
-    // var $addDivBottom = $('#divBottom').children().last();
-    $addDivMain.append('<li>' + ulPeople.name + '</li>');
-    $addDivMain.append('<li>' + ulPeople.git_username + '</li>');
-    $addDivMain.append('<li>' + ulPeople.shoutout + '</li>');
-    console.log(phirephitersArray[3].name);
-    var htmlNavAaccess = document.getElementById('mainImg');
-    // console.log(htmlNavAaccess);
+      function appendDOM(ulPeople) {  // NOTE: Function appending to DOM from AJAX
+        $('#main').empty();
+        $('#main').append('<ul class="ulPeople"></ul>');
+        var $addDivMain = $('#main').children().last();
+        var $addDivNav = $('#navigation').children().last();
+        $addDivMain.append('<li>' + ulPeople.name + '</li>');
+        $addDivMain.append('<li>' + ulPeople.git_username + '</li>');
+        $addDivMain.append('<li>' + ulPeople.shoutout + '</li>');
 
-// QUESTION: How to access thhee NodeList Object? (list of nodes, like an HTML element's collection of child nodes.)
-    //   $('#fwdButton').on('click', function() {
-    // }) // NOTE: FOR: $('#fwdButton').on('click', function() {
+        console.log(phirephitersArray[3].name);
+        var htmlNavAaccess = document.getElementById('navigation');  // NOTE: Need . ?
+        // $addDivNav.append('<li>' + ulPeople.name + '<li>');
+        // console.log(htmlNavAaccess);
+      }; // NOTE: FOR: function appendDOM(ulPeople) {
 
-    for (var i = 0; i < phirephitersArray.length; i++) {
-    $(htmlNavAaccess).mainImg().replaceWith(phirephitersArray[3]);
-      // .replaceWith = HTML string, DOM element, array of DOM elements, or jQuery object.
-    } // NOTE: FOR for loop
-    // console.log($addDivMain); // Returning successfully
-    // $addDivBottom.append('<li>' + phirephitersArray[3] + '</li>'); DELETE? // NOTE: This runs through for Loop
-  } // NOTE: FOR: function appendDOM(ulPeople) {
+
+
+        // QUESTION: How to access thhee NodeList Object? (list of nodes, like an HTML element's collection of child nodes.)
+
+
+
+        // console.log($addDivMain); // Returning successfully
+        // $addDivBottom.append('<li>' + phirephitersArray[3] + '</li>'); DELETE? // NOTE: This runs through for Loop
